f50028977dcd5b4b613835fecfe0f81526d308be
ThisSeanZhang <thisseanzhang@gmail.com>
2020-01-21 01:33:27 +0800

    not yet

--- src/components/command/CommandParam.vue
+++ src/components/command/CommandParam.vue
@@ -20,22 +20,36 @@
         </div>
       </el-popover>
     </div>
-    <div style="display:inline-block;" v-for="para in cparams" :key="para.index">
-      <!-- <div>{{para}}</div> -->
-      <el-popover
-        placement="top"
-        trigger="hover"
-        :content="para.value">
-        <el-tag
-          slot="reference"
-          closable
-          @dblclick.native="doubleClick"
-          draggable="true"
+    <div
+      @drop="drop($event)"
+      @dragover="dragover($event)"
+      >
+      <div style="display:inline-block;"
+        :class="choiceIndex === index ? 'draing-tag' : ''"
+        :draggable="true"
+        @drag.capture="drag($event, index)"
+        @dragstart.capture="dragstart($event, index)"
+        @dragend.capture="dragend(index)"
+        @dragenter.capture="dragenter(index)"
+        @dragleave.capture="dragleave(index)"
+        @dragexit.capture="dragexit"
 
-          type="info">
-          {{para.value}}
-        </el-tag>
-      </el-popover>
+        v-for="(para, index) in comp_paras" :key="para.index">
+        <!-- <div>{{para}}</div> -->
+        <el-popover
+          placement="top"
+          trigger="hover"
+          :content="para.value">
+          <el-tag
+            slot="reference"
+            closable
+
+            @dblclick.native="doubleClick"
+            type="info">
+            {{para.value}}
+          </el-tag>
+        </el-popover>
+      </div>
     </div>
     <el-input
       class="input-new-tag"
@@ -62,9 +76,24 @@ export default {
       type: Array,
     },
   },
+  computed: {
+    comp_paras() {
+      if (this.choiceIndex === undefined) return this.cparams;
+      const [...cparams] = this.cparams;
+      const cParam = cparams.splice(this.choiceIndex, 1)[0];
+      console.log(JSON.stringify(cparams));
+      // cParam.choiced = true;
+      const injectIndex = this.onIndex || this.choiceIndex;
+      console.log(injectIndex);
+      cparams.splice(injectIndex, 0, cParam);
+      return cparams;
+    },
+  },
   data() {
     return {
       cparams: [],
+      choiceIndex: undefined,
+      onIndex: undefined,
       inputValue: null,
       inputVisible: false,
     };
@@ -91,6 +120,50 @@ export default {
       this.inputVisible = false;
       this.inputValue = '';
     },
+
+    // 拖动开始
+    dragstart(event, index) {
+    // event 是被拖动的元素
+      event.dataTransfer.setData('text/plain', event.target);
+      console.log('dragstart', index);
+      this.choiceIndex = index;
+    },
+    // 拖动结束
+    dragend(event) {
+      this.cparams = this.comp_paras;
+      this.onIndex = undefined;
+      this.choiceIndex = undefined;
+      // this.params = params;
+      console.log('dragend', event);
+    },
+    // 拖动到某个块中
+    dragenter(event) {
+      this.onIndex = event;
+      console.log('dragenter', event);
+    },
+    // 拖动到某个
+    dragleave(event) {
+      this.onIndex = event;
+      console.log('dragleave', event);
+    },
+    dragexit() {
+      console.log('exit');
+    },
+    // 当一个元素或是选中的文字被拖拽释放到一个有效的释放目标位置时
+    drag() {
+    // drag(event, index) {
+      // console.log(event.target);
+      // console.log('drag', index);
+      // console.log(event);
+    },
+    drop(event) {
+      event.preventDefault();
+      console.log('drop');
+    },
+    dragover(event) {
+      // console.log(event);
+      event.preventDefault();
+    },
   },
 };
 </script>
@@ -115,5 +188,8 @@ export default {
 }
 .per-option{
   margin-bottom: 10px;
+}
+.draing-tag{
+  opacity: 0.5;
 }
 </style>
