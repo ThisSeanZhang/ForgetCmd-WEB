ca69a5f25fd1f048b8434ffdc6e97f79279c1418
ThisSeanZhang <thisseanzhang@gmail.com>
2020-03-02 07:59:33 +0800

    item exhibit create

---
+++ src/components/commits/ItemExhibit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+import CommitItem from '../../entities/CommitItem';
+
+export default {
+  name: 'item-exhibit',
+  data() {
+    return {
+    };
+  },
+  props: {
+    item: {
+      type: CommitItem,
+      default: () => new CommitItem({}),
+    },
+  },
+  computed: {
+  },
+  methods: {
+  },
+  created() {
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
