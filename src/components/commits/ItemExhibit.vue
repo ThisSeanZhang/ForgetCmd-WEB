1955c505966f8e064a460fb4636b4c18ac2d7ef1
ThisSeanZhang <thisseanzhang@gmail.com>
2020-03-04 18:38:21 +0800

    ...

--- src/components/commits/ItemExhibit.vue
+++ src/components/commits/ItemExhibit.vue
@@ -1,45 +1,87 @@
 <template>
-  <el-card class="box-card modify" shadow="hover">
+  <el-card class="box-card" shadow="hover">
     <!-- <div>
       <el-tag @click="isOpen = !isOpen"
         :type="item.action === 0 ? 'success' : (item.action === 1 ? 'info' : 'danger')"
       >{{item.key}}</el-tag>
       <span></span>
     </div> -->
+    <!-- 动作为创建 -->
     <div v-if="item.action === 0" class="value">
-      <div>
-        <el-tag @click="isOpen = !isOpen" type='success' >{{splitExhibitKey}}:</el-tag>
+      <div @click="isOpen = !isOpen">
+        <el-tag type="success" >{{splitExhibitKey}}</el-tag>
+        <span v-if="!valueIsObj">{{item.value}}</span>
+        <span v-if="valueIsObj" style="margin: 0px 10px;" >{{item.value.briefName}}</span>
+        <span v-if="valueIsObj" style="margin: 0px 10px;" >{{item.value.description.zh}}</span>
+        <i v-if="valueIsObj" :class="isOpen ? 'el-icon-arrow-down' : 'el-icon-arrow-left'"></i>
       </div>
       <div v-if="isObj(item.value)">
         <el-collapse-transition>
-          <div class="transition-box" v-show="isOpen">
-            {{item.value}}
+          <div v-if="item.type === 'options'" class="transition-box" v-show="isOpen">
+            <div v-for="(value, key) in item.value.description" :key="key">
+              {{key}}: {{value}}
+            </div>
+            <div>类型：{{getype(item.value.type)}}</div>
+            <div v-if="isEnum">
+              ENUM 备选值：
+              <el-tag style="margin: 0 5px;"
+                v-for="item in item.value.rules" :key="item" effect="plain">
+                {{ item }}
+              </el-tag>
+            </div>
+            <!-- {{item.value}} -->
+          </div>
+          <div v-if="item.type === 'params'" class="transition-box" v-show="isOpen">
+            <div v-for="(value, key) in item.value.description" :key="key">
+              {{key}}: {{value}}
+            </div>
           </div>
         </el-collapse-transition>
       </div>
-      <span v-else>{{item.value}}</span>
     </div>
+    <!-- 动作为修改 -->
     <div v-else-if="item.action === 1" class="value">
-      <el-tag type='info' >{{item.key}}</el-tag>
+      <el-tag type='info' >{{splitExhibitKey}}:</el-tag>
       <span>{{item.oValue}}</span>=><span>{{item.value}}</span>
     </div>
+    <!-- 动作为删除 -->
     <div v-else-if="item.action === 2" class="value">
-      <div>
-        <el-tag @click="isOpen = !isOpen" type='danger' >{{item.key}}</el-tag>
+      <div  @click="isOpen = !isOpen" >
+        <el-tag type='danger' >{{splitExhibitKey}}</el-tag>
+        <span v-if="!valueIsObj">{{item.oValue}}</span>
+        <span v-if="valueIsObj" style="margin: 0px 10px;" >{{item.oValue.briefName}}</span>
+        <span v-if="valueIsObj" style="margin: 0px 10px;" >{{item.oValue.description.zh}}</span>
+        <i v-if="valueIsObj" :class="isOpen ? 'el-icon-arrow-down' : 'el-icon-arrow-left'"></i>
       </div>
       <div v-if="isObj(item.oValue)">
         <el-collapse-transition>
-          <div class="transition-box" v-show="isOpen">
-            {{item.oValue}}
+          <div v-if="item.type === 'options'" class="transition-box" v-show="isOpen">
+            <div v-for="(desc, key) in item.oValue.description" :key="key">
+              {{key}}: {{desc}}
+            </div>
+            <div>类型：{{getype(item.oValue.type)}}</div>
+            <div v-if="isEnum">
+              ENUM 备选值：
+              <el-tag style="margin: 0 5px;"
+                v-for="item in item.oValue.rules" :key="item" effect="plain">
+                {{ item }}
+              </el-tag>
+            </div>
+            <!-- {{item.oValue}} -->
+          </div>
+          <div v-if="item.type === 'params'" class="transition-box" v-show="isOpen">
+            <div v-for="(oValue, key) in item.oValue.description" :key="key">
+              {{key}}: {{oValue}}
+            </div>
           </div>
         </el-collapse-transition>
       </div>
-      <span v-else>{{item.oValue}}</span>
     </div>
   </el-card>
 </template>
 <script>
 import CommitItem from '../../entities/CommitItem';
+import CommandOption from '../../entities/CommandOption';
 
 export default {
   name: 'item-exhibit',
@@ -55,6 +97,13 @@ export default {
     },
   },
   computed: {
+    isEnum() {
+      return CommandOption.TYPE.ENUM === (
+        this.item.action === 2 ? this.item.oValue.type : this.item.value.type);
+    },
+    cardClass() {
+      return 'box-card delete';
+    },
     valueIsObj() {
       if (this.item.action === 1) return false;
       if (this.item.type === 'base') return false;
@@ -63,11 +112,14 @@ export default {
     },
     splitExhibitKey() {
       if (this.item.type === 'base') return this.item.key;
-      const keys = this.item.key.split('.');
+      const keys = String(this.item.key).split('.');
       if (keys.length >= 2) {
         // TODO need lang map translate
-        return `${keys.shift()} ${keys.join('.')}`;
+        let mainKey = keys.shift();
+        if (this.item.type === 'params') mainKey = `参数${mainKey} : `;
+        return `${mainKey} ${keys.join('.')}`;
       }
+      if (this.item.type === 'params') return `参数${this.item.key + 1}: `;
       return this.item.key;
     },
   },
@@ -83,6 +135,10 @@ export default {
         }
       }
       return type === '[object Object]';
+    },
+    getype(type) {
+      return Object.keys(CommandOption.TYPE).filter(key => CommandOption.TYPE[key] === type)[0] || 'UNKNOW';
+      // return CommandOption.TYPE.filter((_, value) => type === value).map(key => key);
     },
   },
   created() {
@@ -108,12 +164,21 @@ export default {
 .transition-box {
   // height: 100px;
   // border-radius: 4px;
-  box-sizing: border-box;
-  margin-right: 20px;
+  // box-sizing: border-box;
+  // margin-right: 20px;
 }
-.modify{
-  // background-color: #f4f4f5;
+// .modify{
+  //
   // background-color: #f0f9eb;
   // background-color: #f0f9eb;
+// }
+.create {
+//  background-color: #f4f4f5;
+  background-color: #d9f6c9;;
+}
+.delete {
+  background-color: #fef0f0;
+  border-color: #fde2e2;
+  color: #f56c6c;
 }
 </style>
